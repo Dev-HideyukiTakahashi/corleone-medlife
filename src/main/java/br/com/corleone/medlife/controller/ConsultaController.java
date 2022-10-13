1bfee5e7bb6d4c8f411e44a55ce78e9fe559ec5d
Hideyuki Takahashi <yuki.murasaki90@gmail.com>
2022-10-12 23:05:42 -0300

    Consultas view / busca por id

--- src/main/java/br/com/corleone/medlife/controller/ConsultaController.java
+++ src/main/java/br/com/corleone/medlife/controller/ConsultaController.java
@@ -1,10 +1,14 @@
 package br.com.corleone.medlife.controller;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.corleone.medlife.model.entities.Consulta;
 import br.com.corleone.medlife.repository.ConsultaRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -21,5 +25,21 @@ public class ConsultaController {
 
     mv.addObject("consultas", consultaRepository.findAll());
     return mv;
+  }
+
+  @GetMapping(path = "/buscar")
+  public ModelAndView buscarPorId(@RequestParam(name = "id") Long id) {
+    ModelAndView mv = new ModelAndView("/consultas/lista-consultas");
+
+    Optional<Consulta> optional = consultaRepository.findById(id);
+    if (optional.isPresent()) {
+      mv.addObject("consultas", optional.get());
+      mv.addObject("registros", "Consulta localizada.");
+    } else {
+      mv.addObject("registros", "Consulta " + id + " não existe.");
+      mv.addObject("consultas", null);
+    }
+    return mv;
+
   }
 }
